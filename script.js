eea740d523921a438222309c59f550b66a1ed840
Roy9957 <ranobirroy221@gmail.com>
2024-11-06 19:32:15 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to toggle the navigation drawer
+function toggleMenu() {
+    const navDrawer = document.getElementById('navDrawer');
+    navDrawer.style.display = (navDrawer.style.display === 'block') ? 'none' : 'block';
+}
